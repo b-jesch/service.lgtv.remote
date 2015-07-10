d8d0b63926cd99bf9e5259e1ee660a7177c1829b
b-jesch <birger.jesch@gmail.com>
2015-07-10 13:06:49 +0200

    Code sequences redefined

--- default.py
+++ default.py
@@ -13,8 +13,13 @@ __patternSBS__ =  '[-. _]h?sbs[-. _]'
 __patternTAB__ =  '[-. _]h?tab[-. _]'
 
 # Key sequences ROAP/HDCP
-__mode3D_on__ =     {'roap': ['400', '20'], 'hdcp': ['220', '68', '68', '68']}
-__mode3D_off__ =    {'roap': ['400'], 'hdcp': ['220', '68']}
+__mode3DSBS_on__ =     {'roap': ['400', '15', '20'], 'hdcp': ['220', '68', '6', '68']}
+__mode3DSBS_off__ =    {'roap': ['400', '20', '400', '14', '20', '400'], 'hdcp': ['220', '68', '220', '68', '7', '68', '220', '68']}
+__mode3DTAB_on__ =     {'roap': ['400', '15', '15', '20'], 'hdcp': ['220', '68', '6', '6', '68']}
+__mode3DTAB_off__ =    {'roap': ['400', '20', '400', '14', '14', '20', '400'], 'hdcp': ['220', '68', '220', '68', '7', '7', '68', '220', '68']}
+
+__mode3D_on__ = None
+__mode3D_off__ = None
 
 __addon__ = xbmcaddon.Addon()
 __addonname__ = __addon__.getAddonInfo('name')
@@ -50,6 +55,9 @@ class service(xbmc.Player):
         self.Remote = None
         self.getSettings()
 
+        self.isPlaying3D = None
+        self.mode3D = ''
+
     def getSettings(self):
         self.lg_host = __addon__.getSetting('lg_host')
         self.lg_host = None if self.lg_host == '' else self.lg_host
@@ -57,8 +65,6 @@ class service(xbmc.Player):
         self.lg_protocol = __addon__.getSetting('lg_protocol')
         self.lg_protocol = None if self.lg_protocol == __LS__(30017) else self.lg_protocol.lower()
         self.lg_pairing_key = __addon__.getSetting('lg_pairing_key')
-
-        self.isPlaying3D = None
 
         if self.lg_host is not None: self.Remote = interface.Interface(self.lg_host, self.lg_port, self.lg_protocol)
 
@@ -86,24 +92,56 @@ class service(xbmc.Player):
     def onPlayBackStarted(self):
         if self.isPlayingVideo() and self.lg_protocol is not None:
             _file = self.getPlayingFile().decode('utf-8')
-            if re.search(__pattern3D__, _file) and (re.search(__patternSBS__, _file) or re.search(__patternTAB__, _file)):
-                notifyLog('Suggest that %s is a 3D movie.' % (_file))
+            if re.search(__pattern3D__, _file, re.IGNORECASE):
+                if re.search(__patternSBS__, _file, re.IGNORECASE):
+                    self.mode3D = 'SBS'
+                    __mode3D_on__ = __mode3DSBS_on__
+                elif re.search(__patternTAB__, _file, re.IGNORECASE):
+                    self.mode3D = 'TAB'
+                    __mode3D_on__ = __mode3DTAB_on__
+                else: return
+
+                notifyLog('Suggest that %s is a 3D %s movie.' % (_file, self.mode3D))
                 self.sendCommand(__mode3D_on__[self.lg_protocol], self.lg_seq_3D_on)
                 self.isPlaying3D = True
-            else:
+        else:
                 if self.isPlaying3D:
+                    if self.mode3D == 'SBS':
+                        __mode3D_off__ = __mode3DSBS_off__
+                    elif self.mode3D == 'TAB':
+                        __mode3D_off__ = __mode3DTAB_off__
+                    else: return
+
+                    notifyLog('Turn 3D %s mode off' % (self.mode3D))
                     self.sendCommand(__mode3D_off__[self.lg_protocol], self.lg_seq_3D_off)
                     self.isPlaying3D = False
+                    self.mode3D = ''
 
     def onPlayBackStopped(self):
         if self.isPlaying3D:
+            notifyLog('Turn 3D %s mode off' % (self.mode3D))
+            if self.mode3D == 'SBS':
+                __mode3D_off__ = __mode3DSBS_off__
+            elif self.mode3D == 'TAB':
+                __mode3D_off__ = __mode3DTAB_off__
+            else: return
+
             self.sendCommand(__mode3D_off__[self.lg_protocol], self.lg_seq_3D_off)
             self.isPlaying3D = False
+            self.mode3D = ''
 
     def onPlayBackEnded(self):
         if self.isPlaying3D:
+            notifyLog('Turn 3D %s mode off' % (self.mode3D))
+            if self.mode3D == 'SBS':
+                __mode3D_off__ = __mode3DSBS_off__
+            elif self.mode3D == 'TAB':
+                __mode3D_off__ = __mode3DTAB_off__
+            else: return
+
             self.sendCommand(__mode3D_off__[self.lg_protocol], self.lg_seq_3D_off)
             self.isPlaying3D = False
+            self.mode3D = ''
 
 try:
     if sys.argv[1] == 'scan':
