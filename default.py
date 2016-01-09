c19e2660bb49715dcf9de0b0c936cb484646cf0d
Birger Jesch <birger.jesch@gmail.com>
2016-01-09 20:07:42 +0100

    Reload settings if setup was changed while processing Improved handling if a device is disconnected

--- default.py
+++ default.py
@@ -23,6 +23,8 @@ __mode3D_on__ = None
 __mode3D_off__ = None
 
 WAIT_FOR_MODE_SELECT = 60
+WAIT_FOR_NEXT_CONNECT = 60
+POLL_INTERVAL = 1
 
 __addon__ = xbmcaddon.Addon()
 __addonname__ = __addon__.getAddonInfo('name')
@@ -35,20 +37,48 @@ __IconConnected__ = xbmc.translatePath(os.path.join( __path__,'resources', 'medi
 __IconError__ = xbmc.translatePath(os.path.join( __path__,'resources', 'media', 'fail.png'))
 __IconDefault__ = xbmc.translatePath(os.path.join( __path__,'resources', 'media', 'default.png'))
 
+class Monitor(xbmc.Monitor):
+
+    def __init__(self):
+
+        xbmc.Monitor.__init__(self)
+        self.settingsChanged = False
+        self.abortRequested = False
+
+    def onSettingsChanged(self):
+        self.settingsChanged = True
+
+    def onAbortRequested(self):
+        self.abortRequested = True
+
 class Service(xbmc.Player):
 
     def __init__(self):
 
         xbmc.Player.__init__(self)
-        self.Monitor = xbmc.Monitor()
+        self.Mon = Monitor()
         self.Remote = None
+        self.sessionEstablished = False
         self.getSettings()
 
         self.isPlaying3D = None
         self.mode3D = 'OFF'
-        self.modeHasChanged = False
 
-    def getSettings(self):
+    def getSettings(self, init=True):
+        if init: self.readSettings()
+        if self.lg_host is not None:
+            if self.Remote is None:
+                try:
+                    self.Remote = interface.Interface(self.lg_host, self.lg_port, self.lg_protocol)
+                    if not self.Remote.session_id:
+                        self.Remote.get_session_id(self.lg_pairing_key)
+                        self.sessionEstablished = True
+                        tools.notifyLog('Session established. Using session id %s.' % (self.Remote.session_id))
+                except self.Remote.NoConnectionToHostException:
+                    tools.notifyLog('No connection to host on %s' % (self.lg_host), level=xbmc.LOGERROR)
+                    tools.notifyOSD(__addonname__, __LS__(30054), icon=__IconError__)
+
+    def readSettings(self, notify=False):
         self.lg_host = __addon__.getSetting('lg_host')
         self.lg_host = None if self.lg_host == '' else self.lg_host
         self.lg_port = __addon__.getSetting('lg_port')
@@ -56,21 +86,13 @@ class Service(xbmc.Player):
         self.lg_protocol = None if self.lg_protocol == __LS__(30017) else self.lg_protocol.lower()
         self.lg_pairing_key = __addon__.getSetting('lg_pairing_key')
 
-        if self.lg_host is not None:
-            if self.Remote is None:
-                try:
-                    self.Remote = interface.Interface(self.lg_host, self.lg_port, self.lg_protocol)
-                    if not self.Remote.session_id:
-                        self.Remote.get_session_id(self.lg_pairing_key)
-                        tools.notifyLog('Session established. Using session id %s.' % (self.Remote.session_id))
-                except self.Remote.NoConnectionToHostException:
-                    tools.notifyLog('No connection to host on %s' % (self.lg_host), level=xbmc.LOGERROR)
-                    tools.notifyOSD(__addonname__, __LS__(30054), icon=__IconError__)
-
         self.lg_key_delay = int(re.match('\d+', __addon__.getSetting('lg_delay')).group())
         self.lg_own_seqs_enabled = True if __addon__.getSetting('use_own_seq').upper() == 'TRUE' else False
         self.lg_seq_3D_on = ' '.join(__addon__.getSetting('lg_3D_on').replace(',',' ').split()).split()
         self.lg_seq_3D_off = ' '.join(__addon__.getSetting('lg_3D_off').replace(',',' ').split()).split()
+
+        self.Mon.settingsChanged = False
+        if notify: tools.notifyLog('Settings reloaded')
 
     def getStereoscopicMode(self):
         mode = {"off": 'OFF', "split_vertical": 'SBS', "split_horizontal": 'TAB',
@@ -89,7 +111,6 @@ class Service(xbmc.Player):
                 if 'result' in res and 'stereoscopicmode' in res['result']:
                     res = res['result']['stereoscopicmode'].get('mode')
                     if self.mode3D != mode[res]:
-                        self.modeHasChanged = True
                         tools.notifyLog('Stereoscopic mode has changed to %s' % (mode[res]))
                         self.mode3D = mode[res]
                         return True
@@ -118,6 +139,7 @@ class Service(xbmc.Player):
                 tools.notifyLog('Sending keycode %s. Response: %s.' % (code, self.Remote.handle_key_input(code)))
 
         except self.Remote.NoConnectionToHostException:
+            self.sessionEstablished = False
             tools.notifyLog('No connection to host on %s' % (self.lg_host), level=xbmc.LOGERROR)
 
     def onPlayBackStarted(self):
@@ -134,20 +156,7 @@ class Service(xbmc.Player):
                 tools.notifyLog('sending sequence for 3D %s' % (self.mode3D))
                 self.sendCommand(__mode3D_on__[self.lg_protocol], self.lg_seq_3D_on)
                 self.isPlaying3D = True
-        '''
-        else:
-                if self.isPlaying3D:
-                    if self.mode3D == 'SBS':
-                        __mode3D_off__ = __mode3DSBS_off__
-                    elif self.mode3D == 'TAB':
-                        __mode3D_off__ = __mode3DTAB_off__
-                    else: return
 
-                    tools.notifyLog('Turn 3D %s mode off' % (self.mode3D))
-                    self.sendCommand(__mode3D_off__[self.lg_protocol], self.lg_seq_3D_off)
-                    self.isPlaying3D = False
-                    self.mode3D = ''
-        '''
     def onPlayBackStopped(self):
         _currentMode = self.mode3D
         if self.getStereoscopicMode() and self.isPlaying3D and self.mode3D == 'OFF':
@@ -176,30 +185,33 @@ class Service(xbmc.Player):
             self.sendCommand(__mode3D_off__[self.lg_protocol], self.lg_seq_3D_off)
             self.isPlaying3D = False
 
-service = None
+    def poll(self):
+        _host = None if __addon__.getSetting('lg_host') == '' else __addon__.getSetting('lg_host')
+        try:
+            tools.notifyLog('Service running')
 
-try:
-    _host = None if __addon__.getSetting('lg_host') == '' else __addon__.getSetting('lg_host')
-    service = Service()
-    tools.notifyLog('Service running')
+            while not self.Mon.abortRequested:
+                _cycle = POLL_INTERVAL if self.sessionEstablished else WAIT_FOR_NEXT_CONNECT
+                if self.Mon.waitForAbort(_cycle): break
+                if self.Mon.settingsChanged: self.readSettings(notify=True)
+                if not self.sessionEstablished: self.getSettings(init=False)
 
-    while not service.Monitor.abortRequested():
-        if service.Monitor.waitForAbort(1000): break
+        except interface.Interface.LGinNetworkNotFoundException:
+            tools.notifyLog('LG Devices not found in network.', level=xbmc.LOGERROR)
+            tools.dialogOSD( __LS__(30050))
+        except interface.Interface.LGProtocolIssueException:
+            tools.notifyLog('There is an issue with the device protocol.', level=xbmc.LOGERROR)
+            tools.dialogOSD(__LS__(30051))
+        except interface.Interface.LGProtocollNotAcceptedException:
+            tools.notifyLog('Protocol not supported.', level=xbmc.LOGERROR)
+            tools.dialogOSD(__LS__(30052))
+        except interface.Interface.NoConnectionToHostException:
+            tools.notifyLog('No connection to host.', level=xbmc.LOGERROR)
+            tools.dialogOSD(__LS__(30053) % (_host))
+        except Exception, e:
+            pass
 
-except interface.Interface.LGinNetworkNotFoundException:
-    tools.notifyLog('LG Devices not found in network.', level=xbmc.LOGERROR)
-    tools.dialogOSD( __LS__(30050))
-except interface.Interface.LGProtocolIssueException:
-    tools.notifyLog('There is an issue with the device protocol.', level=xbmc.LOGERROR)
-    tools.dialogOSD(__LS__(30051))
-except interface.Interface.LGProtocollNotAcceptedException:
-    tools.notifyLog('Protocol not supported.', level=xbmc.LOGERROR)
-    tools.dialogOSD(__LS__(30052))
-except interface.Interface.NoConnectionToHostException:
-    tools.notifyLog('No connection to host.', level=xbmc.LOGERROR)
-    tools.dialogOSD(__LS__(30053) % (_host))
-except Exception, e:
-    pass
-
-if service is not None: del service
+RemoteService = Service()
+RemoteService.poll()
+del RemoteService
 tools.notifyLog('Service finished')
